d6769d727507675f094e29513e626ea7166107d3
silvergasp <silvergasp@users.noreply.github.com>
2022-06-17 21:42:03 +0000

    fuzz: Adds fuzz tests

---
+++ fuzz/fuzz_targets/init.rs
@@ -0,0 +1,13 @@
+#![no_main]
+use embedded_hal_fuzz::{i2c::I2cFuzz, shared_data::FuzzData};
+use libfuzzer_sys::fuzz_target;
+
+type Error = ();
+
+fuzz_target!(|data: &[u8]| {
+    let data = FuzzData::new(data);
+    let i2c: I2cFuzz<'_, Error> = I2cFuzz::new(data);
+    let pressure_sensor = ms5837::new(i2c);
+    // We ignore the result/error as we only care about potential crashes.
+    let _ = pressure_sensor.init();
+});
---
+++ fuzz/fuzz_targets/read.rs
@@ -0,0 +1,17 @@
+#![no_main]
+use embedded_hal_fuzz::{i2c::I2cFuzz, shared_data::FuzzData};
+use libfuzzer_sys::fuzz_target;
+use ms5837::OverSamplingRatio;
+
+type Error = ();
+
+fuzz_target!(|data: &[u8]| {
+    let data = FuzzData::new(data);
+    let i2c: I2cFuzz<'_, Error> = I2cFuzz::new(data);
+    let pressure_sensor = ms5837::new(i2c);
+    if let Ok(mut pressure_sensor) = pressure_sensor.init() {
+        // We ignore the result as it is likely garbage. We don't care about
+        // the result/error just if it crashes or not.
+        let _ = pressure_sensor.read_temperature_and_pressure(OverSamplingRatio::R4096);
+    }
+});
