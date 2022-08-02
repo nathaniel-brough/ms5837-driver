ba453b6f94542ffbb9356dd62a1b765e4603b131
Nathaniel Brough <nathaniel.brough@gmail.com>
2022-08-02 12:44:09 -0600

    build: Only run dev-dependency bindings for C crc4
    The C implementation for crc4 is used to cross validate the rust
    implementation. We only need to link this in for cases where
    we are testing on host platforms, rather than on target platforms.

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
+use std::env;
+
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=src/crc4.c");
     // Use the `cc` crate to build a C file and statically link it.
-    cc::Build::new().file("src/crc4.c").compile("hello");
+
+    if env::var("CARGO_CFG_UNIX").is_ok() || env::var("CARGO_CFG_WINDOWS").is_ok() {
+        cc::Build::new().file("src/crc4.c").compile("hello");
+    }
 }
